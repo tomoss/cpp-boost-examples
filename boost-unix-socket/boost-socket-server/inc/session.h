311e7ee25cd195840f5725e04fa64e12ef8c7391
Ionut Tomos <ionut.tomoss@gmail.com>
2025-11-29 14:09:04 +0000

    Small improvment

--- boost-unix-socket/boost-socket-server/inc/session.h
+++ boost-unix-socket/boost-socket-server/inc/session.h
@@ -11,17 +11,13 @@ public:
     ~Session();
     static std::shared_ptr<Session> create(boost::asio::io_service& ioService);
     void start();
-
-    void readHeader();
-    void handleReadHeader(const boost::system::error_code& err, size_t bytes_transferred);
-
-    void readBody(std::size_t length);
-    void handleReadBody(const boost::system::error_code& err, size_t bytes_transferred);
-
-    void handleMessage(const std::vector<char>& data);
-
     boost::asio::local::stream_protocol::socket& getSocket();
 private:
+    void readHeader();
+    void handleReadHeader(const boost::system::error_code& err, size_t bytes_transferred);
+    void readBody(std::size_t length);
+    void handleReadBody(const boost::system::error_code& err, size_t bytes_transferred);
+    void handleMessage(const std::vector<char>& data);
     boost::asio::local::stream_protocol::socket m_socket;
     uint32_t m_msgLength = 0;
     std::vector<char> m_buffer;
